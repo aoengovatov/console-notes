5342220abdb97b1236bc3955f61c7294419020b4
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-21 20:30:55 +0400

    add remove note function in console

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-const yargs = require('yargs');
-const pkg = require('./package.json');
-const {addNote, printNotes} = require('./notes.controller');
+const yargs = require("yargs");
+const pkg = require("./package.json");
+const { addNote, printNotes, deleteNoteById } = require("./notes.controller");
 
-yargs.version(pkg.version)
-
-yargs.command({
-        command: 'add',
-        describe: 'Add new note to list',
-        builder: {
-            title: {
-                type: 'string',
-                describe: 'Note title',
-                demandOption: true,
-            }
-        },
-        handler({title}) {
-            addNote(title);
-        },
-})
+yargs.version(pkg.version);
 
 yargs.command({
-    command: 'list',
-        describe: 'Print all notes',
-        async handler() {
-            printNotes();
+    command: "add",
+    describe: "Add new note to list",
+    builder: {
+        title: {
+            type: "string",
+            describe: "Note title",
+            demandOption: true,
         },
-})
+    },
+    handler({ title }) {
+        addNote(title);
+    },
+});
 
-yargs.parse()
+yargs.command({
+    command: "remove",
+    describe: "Remove note by id",
+    builder: {
+        id: {
+            type: "string",
+            describe: "Note ID",
+            demandOption: true,
+        },
+    },
+    handler({ id }) {
+        deleteNoteById(id);
+    },
+});
+
+yargs.command({
+    command: "list",
+    describe: "Print all notes",
+    async handler() {
+        printNotes();
+    },
+});
+
+yargs.parse();
