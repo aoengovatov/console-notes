e8e3d118f453eafd2b0fa963329123765d6644e2
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-21 20:16:12 +0400

    init project

---
+++ notes.controller.js
@@ -0,0 +1,35 @@
+const fs = require('fs/promises');
+const path = require('path');
+const chalk = require('chalk');
+
+const notePath = path.join(__dirname, 'db.json');
+
+async function addNote(title) {
+    const notes = await getNotes();
+
+    const note = {
+        title,
+        id: Date.now().toString()
+    }
+
+    notes.push(note)
+
+    await fs.writeFile(notePath, JSON.stringify(notes));
+    console.log(chalk.bgGreen('Note was added!'))
+}
+
+async function getNotes() {
+    const notes = await fs.readFile(notePath, {encoding: 'utf-8'});
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+}
+
+async function printNotes() {
+    const notes = await getNotes();
+
+    console.log(chalk.bgBlue('Here is the list of notes:'));
+    notes.forEach((note) => console.log(chalk.blue(note.title)));
+}
+
+module.exports = {
+    addNote, printNotes
+}
